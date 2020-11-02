782a49af70813aa7f1e6ba63a1c4c602fbae0f33
Dreu LaVelle <dreu.lavelle@gmail.com@users.noreply.github.com>
2020-11-02 02:42:50 -0700

    Added support for Spotify through GPG keys!

--- setuparch.sh
+++ setuparch.sh
@@ -27,27 +27,46 @@ sudo pacman -S git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -Syu
+sudo yay -Syu
+sudo yay -Yc
 
 # Install Apps (customize this list before running!)
 echo "Installing Your Apps"
 sudo pacman -S terminator kodi zsh zsh-autosuggestions zsh-syntax-highlighting
 echo "Installing Recommended Apps"
-sudo pacman -S vlc libreoffice p7zip p7zip-plugins unrar tar rsync git
+sudo pacman -S vlc libreoffice p7zip p7zip-plugins unrar tar rsync git neofetch
 
 # Install Google Chrome
 echo "Installing Chrome"
 yay -S google-chrome
 
+# Install Spotify (from source - takes a second or two..)
+echo "Installing Spotify"
+sudo pacman -S fakeroot
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
+yay -S spotify
+
+# Install zsh + powerlevel10k
+echo "Installing Zsh + p10k"
+sudo pacman -S zsh
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+sudo chsh $user -s /bin/zsh
+
+# Configuring Zsh
+echo "Adding Zsh Config"
+rm -f ~/.zshrc
+cp Configs/zsh.zshrc ~/.zshrc
+
+# Configuring Neofetch
+echo "Adding Neofetch Config"
+rm -f ~/.config/neofetch/config.conf
+cp Configs/neofetch.conf ~/.config/neofetch/config.conf
+
+# Leave at end-of-file
 # Tidying Up
 echo "Tidying up a little"
 rm -rf ~/.cache/*
-rm -f ~/.local/share/RecentDocuments/*
+rm -rf ~/.local/share/Trash/*
+rm -f ~/.local/share/user-places*
 rm -f ~/rmlint.*
-
-# Install zsh
-sudo pacman -S zsh
-
-# Install powerlevel10k
-yay -S --noconfirm zsh-theme-powerlevel10k-git
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
